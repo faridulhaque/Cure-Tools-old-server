4c566647fc2584cc4e8e914282bf466e5eef5ae8
faridulhaque <faridulhaquemurshed@gmail.com>
2022-05-23 14:58:46 +0600

    getting single item

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-
-// dbuserfm
-// 9OXnr2xLuZPuY4RC
 
 // using middleware
 
 app.use(express.json());
 app.use(cors());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.bygyo.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-async function run(){
-  try{
+async function run() {
+  try {
     await client.connect();
-    const toolsCollection = client.db('cureTools').collection('tools');
-    app.get('/tools', async (req, res) =>{
+    const toolsCollection = client.db("cureTools").collection("tools");
+    app.get("/tools", async (req, res) => {
       const query = {};
       const cursor = toolsCollection.find(query);
       const tools = await cursor.toArray();
       res.send(tools);
-  })
-  }
-  finally{
-
+    });
+    app.get("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
+  } finally {
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) =>{
-    res.send('hello world')
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-app.listen(port, () =>{
-    console.log('listening on port', port);
-})
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
